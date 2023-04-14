ce7b22d7a352c18222137aaf5321ffc10db60589
SaraAlzateC <salzateco@unal.edu.co>
2023-04-13 19:28:03 -0500

    LAB Final

--- pregunta.py
+++ pregunta.py
@@ -20,7 +20,7 @@ def clean_data():
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.upper()
     df['idea_negocio'] = df['idea_negocio'].str.replace('_', ' ').str.replace('-',' ').str.strip().str.upper()
     df['línea_credito'] = df['línea_credito'].str.replace('_', ' ').str.replace('-',' ').str.strip().str.upper()
-    df['barrio'] = df['barrio'].str.replace('_', '-').str.replace('-',' ').str.strip().str.upper()
+    df['barrio'] = df['barrio'].str.lower().str.replace('_', '-').str.replace('-',' ')
     df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '', regex=False).str.replace(',', '').str.replace(' ', '').astype(float)
     df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
     df = df.drop_duplicates().dropna()

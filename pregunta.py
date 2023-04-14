cb4f813be515abd332160312b513c3115cee4b3a
SaraAlzateC <salzateco@unal.edu.co>
2023-04-13 19:08:51 -0500

    Add Sara Test

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,18 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    import pandas as pd
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = df.drop(columns=['Unnamed: 0'])
+    df = df.dropna().drop_duplicates()
+    df['sexo'] = df['sexo'].str.upper()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.upper()
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_', ' ').str.replace('-',' ').str.strip().str.upper()
+    df['línea_credito'] = df['línea_credito'].str.replace('_', ' ').str.replace('-',' ').str.strip().str.upper()
+    df['barrio'] = df['barrio'].str.replace('_', ' ').str.replace('-',' ').str.strip().str.upper()
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '', regex=False).str.replace(',', '').str.replace(' ', '').astype(float)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
+    df = df.drop_duplicates().dropna()
 
     return df
